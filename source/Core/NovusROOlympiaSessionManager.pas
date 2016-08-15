166d94ec5c40f517c63bcf2ed09dc2a8d3968498
support@novuslogic.com <support@novuslogic.com>
2016-08-15 15:02:01 +1000

    New NovusROOlympiaSessionManager Class

--- source/Core/NovusROOlympiaSessionManager.pas
+++ source/Core/NovusROOlympiaSessionManager.pas
@@ -2,7 +2,7 @@ unit NovusROOlympiaSessionManager;
 
 interface
 
-Uses Classes, uROOlympiaSessionManager, uROSessions;
+Uses Classes, uROOlympiaSessionManager, uROSessions, uROTypes, uROClasses;
 
 type
   TNovusROOlympiaSessionManager = class(TROOlympiaSessionManager)
@@ -10,6 +10,8 @@ type
   protected
   public
     function CheckSessionIsExpired(aSession : TROSession) : boolean;
+    function UpdateSession(aSession : TROSession) : boolean;
+    function CreateSession2(aSession : TROSession) : boolean;
   end;
 
 procedure Register;
@@ -20,6 +22,39 @@ function TNovusROOlympiaSessionManager.CheckSessionIsExpired(aSession : TROSessi
 begin
   Result := DoCheckSessionIsExpired(aSession);
 end;
+
+function TNovusROOlympiaSessionManager.UpdateSession(aSession : TROSession) : boolean;
+var
+  data : Binary;
+begin
+  data := Binary.Create;
+  try
+    aSession.SaveToStream(data, TRUE);
+
+    SessionManager.UpdateSession(GUIDToAnsiString(aSession.SessionID), data);
+  finally
+    data.Free;
+  end;
+end;
+
+function TNovusROOlympiaSessionManager.CreateSession2(aSession : TROSession) : boolean;
+var
+  data : Binary;
+  fApplicationID_Ansi: AnsiString;
+begin
+  data := Binary.Create;
+  try
+    aSession.SaveToStream(data, TRUE);
+
+    fApplicationID_Ansi:= GUIDToAnsiString(ApplicationID);
+
+    SessionManager.CreateSession(GUIDToAnsiString(aSession.SessionID),fApplicationID_Ansi, data)
+  finally
+    data.Free;
+  end;
+end;
+
+
 
 procedure Register;
 begin
